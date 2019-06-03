90dccbe282918515c90d877d989297188df5d2b9
Denys Dnishchenko <maden.maxi@yandex.ru>
2019-06-04 02:06:08 +0300

    Init

--- apps/d3nyx/src/app/app.component.ts
+++ apps/d3nyx/src/app/app.component.ts
@@ -1,6 +1,35 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@d3nyx/api-interface';
+import { Book } from '@d3nyx/api-interface';
+
+
+function getRandomArbitrary(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function makeid(length) {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function generateBooks(): Book[] {
+  let books = [];
+  for(let i = 0; i < 1000; i++) {
+    books = books.concat({
+      id: makeid(10),
+      name: `Book #${getRandomArbitrary(10, 200)}`,
+      authors: ['Author1', 'Author2'],
+      year: getRandomArbitrary(1900, 2000),
+      price: getRandomArbitrary(10, 200),
+      description: 'Description'
+    });
+  }
+  return books;
+}
 
 @Component({
   selector: 'd3nyx-root',
@@ -8,6 +37,12 @@ import { Message } from '@d3nyx/api-interface';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  books = generateBooks();
+  headers = [
+    {name: 'name', title: 'Name'},
+    {title: 'Authors'},
+    {name: 'year', title: 'Year'},
+    {name: 'price', title: 'Price'},
+    {title: 'Description'},
+  ];
 }
